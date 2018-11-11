07a592adbfd084a6373accf80dfe357385703453
Daniel <dmockaitis@att.net>
2018-11-11 00:40:03 -0800

    Farhad doesn't work

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,30 +1,23 @@
 import React from 'react';
 import { render } from 'react-dom';
-
-import './index.css';
-import Index from './pages/index';
-<<<<<<< HEAD
-import Cards from './pages/cards';
-import LandingPage from './pages/landing-page';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import Index from './pages/index';
+import Discover from './components/Discover/Discover';
+import Cards from './pages/cards';
+import LandingPage from './pages/landing-page';
+
 import './app.css'
+import './index.css';
 
 render((
   <BrowserRouter>
     <Switch>
       <Route exact path='/' component={LandingPage}/>
       <Route exact path='/chain' component={Index}/>
+      <Route exact path='/discover' component={Discover}/>
       <Route exact path='/cards' component={Cards}/>
     </Switch>
 
   </BrowserRouter>
 ), document.getElementById('root'))
-=======
-import Disover from './components/Discover/Discover';
-
-render(
-  <Disover />,
-  document.getElementById('root')
-);
->>>>>>> 6a2a4861358743e7eae2dcf9715012e350ce6580
