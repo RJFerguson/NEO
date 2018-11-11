1e3fe3560d30b3a62cf207b88d1d63afbc9b096b
Ryan Ferguson <fergusman@gmail.com>
2018-11-11 05:04:01 -0800

    wasm methods we need, still having touble compiling

--- eosio_docker/contracts/neohash/neohash.cpp
+++ eosio_docker/contracts/neohash/neohash.cpp
@@ -3,24 +3,28 @@
 using namespace eosio;
 
 CONTRACT neohash : public eosio::contract
-
-                       TABLE neostruct
 {
-  uint64_t prim_key; // primary key
-  name user;
-  std::string neodata;
-  uint64_t timestamp;
+  TABLE neostruct
+  {
+    uint64_t prim_key; // primary key
+    name user;
+    std::string neodata;
+    uint64_t timestamp;
 
-  // primary key
-  auto primary_key() const { return prim_key; }
-};
+    // primary key
+    auto primary_key() const { return prim_key; }
+
+    uint64_t get_by_time() const { return timestamp; }
+  };
 
 // create a multi-index table and support secondary key
-typedef eosio::multi_index<name("neotruct"), neostruct> neo_table;
+  typedef eosio::multi_index<name("neostruct"), neostruct, indexed_by<name("getbytime"), const_mem_fun<neostruct, uint64_t, &neostruct::get_by_time>>> 
+    neo_table;
 
-neo_table _neo;
 
-{
+  neo_table  _neo;
+
+
 public:
   using contract::contract;
 
@@ -32,7 +36,33 @@ public:
     print("Hello, ", name{user});
   }
 
+  ACTION update(name user, std::string & neodata)
+  {
+    // to sign the action with the given account
+    require_auth(user);
+    {
+      // get object by secordary key
+      auto neo_index = _neo.get_index<name("getbytime")>();
+      auto &note_entry = neo_index.get(timestamp);
+      // update existing note
+      _notes.modify(note_entry, _self, [&](auto &modified_user) {
+        modified_user.note = note;
+        modified_user.timestamp = now();
+      });
+    }
+  }
+
   ACTION submithash(name user, std::string & neodata)
+  {
+    _neo.emplace(_self, [&](auto &user) {
+      user.prim_key = _neo.available_primary_key();
+      user.user = user;
+      user.neodata = neodata;
+      user.timestamp = now();
+    });
+  }
+
+  ACTION findHash(name user, std::string & neodata)
   {
     _neo.emplace(_self, [&](auto &user) {
       user.prim_key = _neo.available_primary_key();
